1d46ce3fdbb23b50b3c2f1a3a0d72d9a04cc8399
gourabbl <gourabbal6@gmail.com>
2024-03-07 22:51:59 +0530

    .

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Navbar from './components/Navbar'
+import Home from './components/Home'
+
+function App() {
+  
+
+  return (
+    <h1>
+      <Navbar/>
+      <Home />
+    </h1>
+  )
+}
+
+export default App
---
+++ src/components/Home.jsx
@@ -0,0 +1,21 @@
+const Home = () => {
+    return (
+        <div className="md:px-12 p-4 max-2-screen-2xl mx-auto mt-28">
+            <div>
+                <div>
+                    <div>
+                        <h2>Develop your skills without diligence.</h2>
+                        <p>A good example of a paragraph contains a topic sentence, details and a conclusion. 
+                        There are many different kinds of animal that live in china</p>
+                    </div>
+                    <div>
+                        <button>Get Started</button>
+                        <button>Discount</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
